54e331778f352bbed72807e46d116a45f41c3e26
Gustavo Ibarra <gus@aufacicenta.com>
2022-01-22 13:39:13 -0600

    feat: property content. WIP

--- app/src/app/property-details/PropertyDetails.module.scss.d.ts
+++ app/src/app/property-details/PropertyDetails.module.scss.d.ts
@@ -1,5 +1,23 @@
 export type Styles = {
   "property-details": string;
+  "property-details__attachments": string;
+  "property-details__attachments--card": string;
+  "property-details__attachments--content": string;
+  "property-details__attachments--overlay": string;
+  "property-details__attachments--overlay-text": string;
+  "property-details__content": string;
+  "property-details__details": string;
+  "property-details__location": string;
+  "property-details__location--map": string;
+  "property-details__location--overlay": string;
+  "property-details__location--overlay-text": string;
+  "property-details__main": string;
+  "property-details__media": string;
+  "property-details__media--single-image": string;
+  "property-details__ownership--card": string;
+  "property-details__register-interest-modal--header": string;
+  "property-details__sidebar": string;
+  "property-details__sidebar--heading": string;
   "z-depth-0": string;
   "z-depth-1": string;
   "z-depth-1-half": string;
