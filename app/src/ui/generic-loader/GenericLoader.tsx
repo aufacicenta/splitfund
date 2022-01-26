c1b6300f0acbed28e001aaad0e0db0e153f74351
Gustavo Ibarra <gus@aufacicenta.com>
2022-01-26 16:15:03 -0600

    chore: changes logo

--- app/src/ui/generic-loader/GenericLoader.tsx
+++ app/src/ui/generic-loader/GenericLoader.tsx
@@ -1,14 +1,14 @@
 import clsx from "clsx";
+
+import { NearHoldingsIcon } from "ui/icons/NearHoldingsIcon";
+
 import { GenericLoaderProps } from "./GenericLoader.types";
 import styles from "./GenericLoader.module.scss";
-import { BancoSatoshiLogo } from "ui/icons/BancoSatoshiLogo";
 
-export const GenericLoader: React.FC<GenericLoaderProps> = ({ className }) => {
-  return (
-    <div className={clsx(styles["generic-loader"], className)}>
-      <div className={clsx(styles["generic-loader__logo"], className)}>
-        <BancoSatoshiLogo />
-      </div>
+export const GenericLoader: React.FC<GenericLoaderProps> = ({ className }) => (
+  <div className={clsx(styles["generic-loader"], className)}>
+    <div className={clsx(styles["generic-loader__logo"], className)}>
+      <NearHoldingsIcon />
     </div>
-  );
-};
+  </div>
+);
