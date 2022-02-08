0a96f080c462f354af310e661ae092e30b30f027
Gustavo Ibarra <gus@aufacicenta.com>
2022-02-08 13:03:09 -0600

    feat(Home): fasst UI

---
+++ app/src/app/home/home/Home2.module.scss.d.ts
@@ -0,0 +1,35 @@
+export type Styles = {
+  button_primary: string;
+  button_primary__icon: string;
+  button_primary__icon_left: string;
+  button_primary__icon_right: string;
+  button_primary_invert: string;
+  button_primary_large: string;
+  button_primary_medium: string;
+  home: string;
+  "home__featured-assets": string;
+  home__intro: string;
+  "home__intro--box": string;
+  "home__intro--cta": string;
+  "home__intro--headline": string;
+  "home__intro--image": string;
+  "home__intro--linear-gradient": string;
+  "home__intro--text-block": string;
+  "home__powered-by": string;
+  home__section: string;
+  "home__what-is": string;
+  "home__what-is--row": string;
+  "z-depth-0": string;
+  "z-depth-1": string;
+  "z-depth-1-half": string;
+  "z-depth-2": string;
+  "z-depth-3": string;
+  "z-depth-4": string;
+  "z-depth-5": string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
--- app/src/ui/footer/Footer.tsx
+++ app/src/ui/footer/Footer.tsx
@@ -5,6 +5,9 @@ import { Grid } from "ui/grid/Grid";
 import { Typography } from "ui/typography/Typography";
 import { Icon } from "ui/icon/Icon";
 import { useRoutes } from "hooks/useRoutes/useRoutes";
+import { NearHoldingsIcon } from "ui/icons/NearHoldingsIcon";
+import { NearLogoHorizontal } from "ui/icons/NearLogoHorizontal";
+import { AufacicentaIcon } from "ui/icons/Aufacicentaicon";
 
 import styles from "./Footer.module.scss";
 import { FooterProps } from "./Footer.types";
@@ -19,16 +22,28 @@ export const Footer: React.FC<FooterProps> = ({ className }) => {
         <Grid.Container>
           <Grid.Row>
             <Grid.Col lg={7} xs={12} sm={6}>
-              <Typography.Headline4 className={styles["footer__cta-banner--welcome"]}>
-                <Trans>{t("intro.bottomBanner.welcome")}</Trans>
-              </Typography.Headline4>
-              <Typography.Text className={styles["footer__cta-banner--description"]}>
-                <Trans>{t("intro.bottomBanner.description")}</Trans>
-              </Typography.Text>
+              <Typography.Link href={routes.home} className={styles["footer__cta-banner--icon"]}>
+                <NearHoldingsIcon theme="dark" />
+              </Typography.Link>
             </Grid.Col>
+          </Grid.Row>
+        </Grid.Container>
+        <Grid.Container>
+          <Grid.Row>
             <Grid.Col lg={5} xs={12} sm={6}>
-              <Typography.Link className={styles["footer__cta-banner--cta"]} href={routes.invest.grid}>
-                {t("intro.bottomBanner.cta")} <Icon name="icon-chevron-right-circle" />
+              <Typography.TextLead className={styles["footer__cta-banner--description"]}>
+                <Trans>{t("intro.bottomBanner.description")}</Trans>
+              </Typography.TextLead>
+            </Grid.Col>
+            <Grid.Col lg={7} xs={12} sm={6}>
+              <Typography.Link className={styles["footer__cta-banner--link"]} href={routes.invest.grid}>
+                FAQs
+              </Typography.Link>
+              <Typography.Link className={styles["footer__cta-banner--link"]} href={routes.invest.grid}>
+                Asset Explorer
+              </Typography.Link>
+              <Typography.Link className={styles["footer__cta-banner--link"]} href={routes.invest.grid}>
+                About Us
               </Typography.Link>
             </Grid.Col>
           </Grid.Row>
@@ -38,13 +53,34 @@ export const Footer: React.FC<FooterProps> = ({ className }) => {
         <Grid.Container>
           <Grid.Row>
             <Grid.Col lg={7} xs={6} sm={6}>
-              <Typography.Text flat>© NEAR Holdings 2022</Typography.Text>
+              <div className={styles["footer__powered-by"]}>
+                <div>
+                  <Typography.Text flat>{t("poweredBy", { ns: "common" })}</Typography.Text>
+                </div>
+                <div>
+                  <NearLogoHorizontal />
+                </div>
+              </div>
             </Grid.Col>
             <Grid.Col lg={5} xs={6} sm={6}>
               <div className={styles["footer__copyright--social"]}>
-                <Typography.Anchor flat href="https://github.com/aufacicenta/near.holdings-web" target="_blank">
-                  <Icon name="icon-github" className={styles["footer__copyright--social-icon"]} />
-                </Typography.Anchor>
+                <Grid.Row>
+                  <Grid.Col width="auto">
+                    <Typography.Text flat className={styles["footer__copyright--social-text"]}>
+                      © NEAR Holdings 2022
+                    </Typography.Text>
+                  </Grid.Col>
+                  <Grid.Col width="auto">
+                    <Typography.Anchor flat href="https://github.com/aufacicenta/near.holdings-web" target="_blank">
+                      <AufacicentaIcon className={styles["footer__copyright--social-aufacicenta-icon"]} />
+                    </Typography.Anchor>
+                  </Grid.Col>
+                  <Grid.Col width="auto">
+                    <Typography.Anchor flat href="https://github.com/aufacicenta/near.holdings-web" target="_blank">
+                      <Icon name="icon-github" className={styles["footer__copyright--social-icon"]} />
+                    </Typography.Anchor>
+                  </Grid.Col>
+                </Grid.Row>
               </div>
             </Grid.Col>
           </Grid.Row>
