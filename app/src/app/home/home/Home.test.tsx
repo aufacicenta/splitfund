37bf6ec3a8956da3841cf7ee8b3febbe87fc531d
Gustavo Ibarra <gus@aufacicenta.com>
2022-02-09 08:27:49 -0600

    chore: removes legacy Home component

--- app/src/app/home/home/Home.test.tsx
+++ app/src/app/home/home/Home.test.tsx
@@ -1,12 +1,12 @@
 import { screen, render } from "tests";
 
-import { Home } from "./Home";
+import { Home2 } from "./Home2";
 
-describe("Home", () => {
+describe("Home2", () => {
   it("renders children correctly", () => {
-    render(<Home>Home</Home>);
+    render(<Home2>Home2</Home2>);
 
-    const element = screen.getByText("Home");
+    const element = screen.getByText("Home2");
 
     expect(element).toBeInTheDocument();
   });
