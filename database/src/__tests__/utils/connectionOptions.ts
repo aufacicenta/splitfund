a4874d8bc51c7e8b8d59bd3c8b71af5a0e20de43
Gustavo Ibarra <gus@aufacicenta.com>
2022-02-23 11:32:41 -0600

    chore: removes mcs directories

--- database/src/__tests__/utils/connectionOptions.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
-  user: process.env.MYSQL_ROOT_USER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-};
--- database/src/dao/business/create.ts
+++
@@ -1,7 +0,0 @@
-import { ModelCtor } from "sequelize/dist";
-import { BusinessModel } from "../../business/model";
-import { BusinessDAO } from "../types";
-
-export default (model: ModelCtor<BusinessModel>): BusinessDAO["business"]["create"] => {
-  return async (args) => await model.create(args, { logging: false });
-};
