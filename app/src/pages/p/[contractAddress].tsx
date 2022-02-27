3cd966df8ed896f3195306114c6e133788e0769b
Gustavo Ibarra <gus@aufacicenta.com>
2022-02-27 17:00:50 -0600

    fix: removes i18n load on dev

--- app/src/pages/p/[contractAddress].tsx
+++ app/src/pages/p/[contractAddress].tsx
@@ -1,5 +1,4 @@
 import { GetServerSidePropsContext, NextPage } from "next";
-import { i18n } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import * as nearAPI from "near-api-js";
 import { PropertyCard } from "api/codegen";
@@ -40,8 +39,6 @@ export async function getServerSideProps({ params, locale }: GetServerSidePropsC
   const conditionalEscrow = new ConditionalEscrow(contract);
   const metadataUrl = await conditionalEscrow.getMetadataUrl();
   const property = await ConditionalEscrow.getPropertyFromMetadataUrl(metadataUrl);
-
-  await i18n?.reloadResources();
 
   return {
     props: { property, ...(await serverSideTranslations(locale!, ["common", "head"])) },
